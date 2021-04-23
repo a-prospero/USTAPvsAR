aeef1c33e1abb8001a34bb97f4bedef853be21d6
april Prospero Garciagit config --global user.email april.prospero@gmail.com <april.prospero@gmail.com>
2021-04-23 14:55:41 -0600

    upload

---
+++ APvsAR21.js
@@ -0,0 +1,49 @@
+// var arDue20 = {
+//   x: ["January", "February", "March","April", "May", "June","July", "August", "September","October", "November", "December"],
+//   y: [3418163, 2895239, 3618074, 3965922, 3702453, 3819666, 3858723, 3699495, 3744365,3694123, 3537986,3242059],
+//   mode: 'lines+markers+text',
+//   name: '2020 Processed AR',
+//   type: "line"
+// };
+
+// // Create our second trace
+// var apDue20 = {
+//   x: ["January", "February", "March","April", "May", "June","July", "August", "September","October", "November", "December"],
+//   y: [2535453, 1994062, 2046836, 1781241, 2230542,2778134, 2364457, 2340205, 2203273, 2179594, 1875376, 1801086],
+//   mode: 'lines+markers+text',
+//   name: '2020 Processed AP',
+//   type: "line"
+// };
+
+
+
+var arDue = {
+    x: ["January", "February", "March"],
+    y: [3832595, 3432667, 1239205],
+    mode: 'lines+markers+text',
+    name: '2021 Processed AR',
+    type: "scatter"
+  };
+  
+  // Create our second trace
+  var apDue = {
+    x: ["January", "February", "March"],
+    y: [2135247, 1913709, 2489292],
+    mode: 'lines+markers+text',
+    name: '2021 Processed AP',
+    type: "scatter"
+  };
+
+
+
+  
+
+  
+  // The data array consists of both traces
+  var data = [arDue, apDue];
+ 
+  
+  // Note that we omitted the layout object this time
+  // This will use default parameters for the layout
+  Plotly.newPlot("plot", data);
+ 
---
+++ bar.js
@@ -0,0 +1,29 @@
+var month = ["January", "February", "March","April", "May", "June","July", "August", "September","October", "November", "December"];
+var ar = [3418163, 2895239, 3618074, 3965922, 3702453, 3819666, 3858723, 3699495, 3744365,3694123, 3537986,3242059];
+var apDue = [2535453, 1994062, 2046836, 1781241, 2230542,2778134, 2364457, 2340205, 2203273, 2179594, 1875376, 1801086];
+
+// Create the Trace
+var trace1 = {
+  x: month,
+  y: ar,
+  type: "bar"
+};
+
+var trace2 = {
+  x: month,
+  y: apDue,
+  type: "bar"
+};
+
+// Create the data array for the plot
+var data = [trace1, trace2];
+
+// Define the plot layout
+var layout = {
+  title: "AP vs AR 2020",
+  xaxis: { title: "Month"},
+  yaxis: { title: "Amount"}
+};
+
+// Plot the chart to a div tag with id "bar-plot"
+Plotly.newPlot("bar-plot", data, layout);
